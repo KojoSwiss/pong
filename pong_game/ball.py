3bbd42bdae8d43cc3c0a3f25f8dcb32d7ce9681e
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-21 11:24:26 +0100

    Pong complete

--- pong_game/ball.py
+++ pong_game/ball.py
@@ -1,14 +1,17 @@
 from turtle import Turtle
+import time
 
 
 class Ball(Turtle):
+
     def __init__(self):
         super().__init__()
-        self.shape("circle")
         self.color("white")
+        self.shape("circle")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -20,3 +23,10 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
+        time.sleep(1)
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
+
     def __init__(self, position):
         super().__init__()
         self.shape("square")
@@ -17,4 +18,3 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
-
